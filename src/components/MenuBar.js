b355e7e93591e13c2b24fd48dbfb47c20e0b0fd7
naterg2000 <naterg2000@gmail.com>
2024-03-10 17:47:09 -0700

    initial commit

---
+++ src/components/MenuBar.js
@@ -0,0 +1,15 @@
+const MenuBar = () => {
+    return(
+        <div className="border h-10 w-8">
+            this is the menu bar. This will contain every other top banner element
+            <div>
+                logo goes here
+            </div>
+            <div>
+                item categories go here
+            </div>
+        </div>
+    );
+};
+
+export default MenuBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,19 @@
 import type { Config } from "tailwindcss";
 
+const colors = require('tailwindcss/colors')
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: "jit",
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', '..public/intex.html'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gray: colors.gray,
       },
     },
   },
